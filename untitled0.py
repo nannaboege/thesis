556e2d30ccb7b99caeb90fd694100f22aeacedb3
nannaboege <33054724+nannaboege@users.noreply.github.com>
2019-02-04 09:50:24 +0100

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 10:23:53 2019
+
+@author: nanna
+"""
+
+#%%
+import pandas as pd
+data=pd.read_csv('dataset.csv', sep=';', encoding='ANSI')
+data_text=data['s_31']
+data_text.to_csv('d.csv')
+#%%
+
+#%%
+import spacy
+spacy.load('en')
+from spacy.lang.en import English
+parser = English()
+def tokenize(text):
+    lda_tokens = []
+    tokens = parser(text)
+    for token in tokens:
+        if token.orth_.isspace():
+            continue
+        elif token.like_url:
+            lda_tokens.append('URL')
+        elif token.orth_.startswith('@'):
+            lda_tokens.append('SCREEN_NAME')
+        else:
+            lda_tokens.append(token.lower_)
+    return lda_tokens
+#%%
+
+import nltk
+nltk.download('wordnet')
+from nltk.corpus import wordnet as wn
+
+def get_lemma(word):
+    lemma = wn.morphy(word)
+    if lemma is None:
+        return word
+    else:
+        return lemma
+#%%    
+#from nltk.stem.wordnet import WordNetLemmatizer
+#
+#def get_lemma2(word):
+#    return WordNetLemmatizer().lemmatize(word)
+
+#%%
+nltk.download('stopwords')
+en_stop = set(nltk.corpus.stopwords.words('danish'))
+
+#%%
+import nltk
+def prepare_text_for_lda(text):
+    tokens = nltk.word_tokenize(text)
+    tokens = [token for token in tokens if len(token) > 4]
+    #tokens = [token for token in tokens if token not in en_stop]
+    tokens = [get_lemma(token) for token in tokens]
+    return tokens
+
+
+#%%
+import random
+
+
+text_data = []
+with open('d.csv', encoding='utf-8') as f:
+    for line in f:
+        tokens = prepare_text_for_lda(line)
+        #if random.random() > .99:
+        print(tokens)
+        text_data.append(tokens)
+#%%
+text_data
+
+
+#%%
+from gensim import corpora
+dictionary = corpora.Dictionary(text_data)
+corpus = [dictionary.doc2bow(text) for text in text_data]
+#%%
+import pickle
+pickle.dump(corpus, open('corpus.pkl', 'wb'))
+dictionary.save('dictionary.gensim')
+
+#%%
+import gensim
+NUM_TOPICS = 5
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = NUM_TOPICS, id2word=dictionary, passes=15)
+ldamodel.save('model5.gensim')
+topics = ldamodel.print_topics(num_words=4)
+for topic in topics:
+    print(topic)
